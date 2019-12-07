2bc6905b012b80de2226b2bb3707ae99e3f14497
Shyam Gorai <spg.144@gmail.com>
2019-12-08 02:06:42 +0530

    Code modified

--- src/main/java/com/salesorder/microservice/customerservice/CustomerController.java
+++ src/main/java/com/salesorder/microservice/customerservice/CustomerController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class CustomerController {
@@ -23,17 +25,21 @@ public class CustomerController {
 	@PostMapping("/customer")
 	public Long createOrder(@RequestBody Customer  customer){
 		List<Customer> list = getAllCustomers();
+		long id = generateId(list);
+		customer.setId(id);
+		customerRepository.save(customer);
+		return id;
+		
+	}
+
+	private long generateId(List<Customer> list) {
 		long id =0;
 		for(Customer c : list) {
 			if(id < c.getId()) {
 				id=c.getId();
 			}
 		}
-		id++;
-		customer.setId(id);
-		customerRepository.save(customer);
-		return id;
-		
+		return id+1;
 	}
 	
 
